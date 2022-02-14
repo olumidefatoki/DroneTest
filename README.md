58ad9bdc2d08d0b71a9f9b99555367e7f3a15624
olumide fatoki <olumide.fatoki@gmail.com>
2022-02-14 09:41:06 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -17,13 +17,14 @@ Api was designed using Springboot framework and H2 as the database.
 
 
 Below are the api for various services
+
 - registering a drone : /9092/api/v1/drone;
 - loading a drone with medication items : /api/v1/drone/medication/load
 - checking loaded medication items for a given drone : /api/v1/drone/medication/{serialNumber} 
 - checking available drones for loading : /api/v1/drone/available
 - check drone battery level for a given drone : /api/v1/drone/batteryLevel/1234567890
 
-
+to get preloaded Drone Data : /api/v1/drone/all
 
 
 :scroll: **END**
--- src/main/java/com/musala/drone/DroneApplication.java
+++ src/main/java/com/musala/drone/DroneApplication.java
@@ -1,13 +1,108 @@
 package com.musala.drone;
 
+import com.musala.drone.domain.request.DroneMedicationRequest;
+import com.musala.drone.domain.request.MedicationRequest;
+import com.musala.drone.enums.DroneModel;
+import com.musala.drone.enums.DroneState;
+import com.musala.drone.model.Drone;
+import com.musala.drone.model.Medication;
+import com.musala.drone.repo.DroneRepo;
+import com.musala.drone.repo.MedicationRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DroneApplication {
+public class DroneApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DroneApplication.class, args);
-	}
+    @Autowired
+    DroneRepo droneRepo;
 
+    @Autowired
+	MedicationRepo medicationRepo;
+
+    @Autowired
+	ModelMapper mapper;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Drone drone = new Drone();
+        drone.setSerialNumber("abc_001");
+        drone.setModel(DroneModel.LIGHTWEIGHT.toString());
+        drone.setWeight(200);
+        drone.setBatteryCapacity(100);
+        drone.setState(DroneState.IDLE.toString());
+        droneRepo.save(drone);
+
+        drone = new Drone();
+        drone.setSerialNumber("abc_002");
+        drone.setModel(DroneModel.LIGHTWEIGHT.toString());
+        drone.setWeight(200);
+        drone.setBatteryCapacity(100);
+        drone.setState(DroneState.IDLE.toString());
+        droneRepo.save(drone);
+
+		drone  = new Drone();
+		drone.setSerialNumber("abd_001");
+		drone.setModel(DroneModel.CRUISERWEIGHT.toString());
+		drone.setWeight(200);
+		drone.setBatteryCapacity(100);
+		drone.setState(DroneState.IDLE.toString());
+		droneRepo.save(drone);
+
+		drone  = new Drone();
+		drone.setSerialNumber("abd_002");
+		drone.setModel(DroneModel.CRUISERWEIGHT.toString());
+		drone.setWeight(200);
+		drone.setBatteryCapacity(100);
+		drone.setState(DroneState.IDLE.toString());
+		droneRepo.save(drone);
+
+		drone  = new Drone();
+		drone.setSerialNumber("abe_001");
+		drone.setModel(DroneModel.MIDDLEWEIGHT.toString());
+		drone.setWeight(200);
+		drone.setBatteryCapacity(100);
+		drone.setState(DroneState.IDLE.toString());
+		droneRepo.save(drone);
+
+		drone  = new Drone();
+		drone.setSerialNumber("abe_002");
+		drone.setModel(DroneModel.MIDDLEWEIGHT.toString());
+		drone.setWeight(200);
+		drone.setBatteryCapacity(100);
+		drone.setState(DroneState.IDLE.toString());
+		droneRepo.save(drone);
+
+		drone  = new Drone();
+		drone.setSerialNumber("abf_001");
+		drone.setModel(DroneModel.HEAVYWEIGHT.toString());
+		drone.setWeight(200);
+		drone.setBatteryCapacity(100);
+		drone.setState(DroneState.IDLE.toString());
+		droneRepo.save(drone);
+
+		drone  = new Drone();
+		drone.setSerialNumber("abf_002");
+		drone.setModel(DroneModel.HEAVYWEIGHT.toString());
+		drone.setWeight(200);
+		drone.setBatteryCapacity(100);
+		drone.setState(DroneState.IDLE.toString());
+		droneRepo.save(drone);
+
+		MedicationRequest medicationRequest  = new MedicationRequest();
+		medicationRequest.setWeight(2.5);
+		medicationRequest.setCode("PARA_001");
+		medicationRequest.setName("PARACETAMOL");
+		Medication medication = mapper.map(medicationRequest, Medication.class);
+		medication.setDroneId(1);
+		medicationRepo.save(medication);
+    }
 }
