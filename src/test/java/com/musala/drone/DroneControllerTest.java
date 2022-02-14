6aa00a1a4f876b817544bfb39955d4ec0d0a951c
olumide fatoki <olumide.fatoki@gmail.com>
2022-02-14 12:10:23 +0100

    Added test for Create controller.

--- src/test/java/com/musala/drone/DroneControllerTest.java
+++ src/test/java/com/musala/drone/DroneControllerTest.java
@@ -1,46 +1,53 @@
 package com.musala.drone;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musala.drone.controller.DispatchController;
 import com.musala.drone.domain.request.CreateDroneRequest;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-class DroneApplicationTests {
-    protected MockMvc mvc;
-    @Autowired
-    WebApplicationContext webApplicationContext;
+public class DroneControllerTest {
+    private MockMvc mvc;
 
-    protected void setUp() {
-        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    @Autowired
+    DispatchController controller;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
     }
+
 
     @Test
     public void createDroneSuccessfully() throws Exception {
-
         ObjectMapper objectMapper = new ObjectMapper();
         CreateDroneRequest request = new CreateDroneRequest();
         request.setModel("LIGHTWEIGHT");
         request.setBatteryCapacity(100);
         request.setWeight(Double.parseDouble("20"));
-        request.setSerialNumber("1234567899");
+        request.setSerialNumber("1234567890");
 
 
-
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/api/v1/dispatch")
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/api/v1/drone")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(objectMapper.writeValueAsBytes(request))).andReturn();
 
         String content = mvcResult.getResponse().getContentAsString();
         System.out.println(content);
-        Assert.assertEquals(content, "Drone successfully created");
+        Assert.assertTrue(content.contains("Drone successfully created"));
     }
 }
